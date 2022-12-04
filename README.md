27ae04f975dce7eb2db92a9de5f6d83c728a9209
Dr. Jones <jonathan.matthew.holmes@gmail.com>
2022-12-04 00:25:20 -0500

    as always, adding in error printing first

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 ### AoC (not that one) 2022!!!
 
-Doing this in C, on Windows, as a Visual Studio project for the laughs
+Doing this in C, on Windows, as a Visual Studio (2019) project for the laughs
 
 Starting 3/12/2022, so we're a bit behind schedule
+
+#### Day 1 
+
+First thoughts; god this would be easier to do in Python
+
--- adventOfCode/Source.c
+++ adventOfCode/Source.c
@@ -1,9 +1,58 @@
 #include <Windows.h>
+#include <stdio.h>
+//
+// need to be static for REASONS
+// 0x1000 = 4096 = 1 page (ok not always but always)
+//
+#define BUFFERSIZE 0x1000
+
+void errorPrinter(LPTSTR printer)
+{
+	DWORD dwError = 0;
+
+	dwError = GetLastError();
+	printf("%ls\n", printer);
+	printf("ERROR\t%d\n", dwError);
+
+}
 
 int main() {
 	DWORD retVal = 0;
+	BOOL bCheck = FALSE;
+	DWORD dwBytesRead = 0;
+	LPDWORD pBytesRead = &dwBytesRead;
+	LPVOID lpBuffer = malloc(BUFFERSIZE);
+	DWORD dwCurrentSize = 0;
+	HANDLE hFile = INVALID_HANDLE_VALUE;
+
+	if (lpBuffer == NULL)
+	{
+		errorPrinter(L"malloc error!");
+		goto exit;
+	}
+	RtlSecureZeroMemory(lpBuffer, BUFFERSIZE); // not strictly necessary but whatever
+
+	hFile = CreateFile(
+		L"../XXXInput/day1pt1.txt",
+		GENERIC_READ,
+		FILE_SHARE_READ,
+		NULL,
+		OPEN_EXISTING,
+		FILE_ATTRIBUTE_NORMAL,
+		NULL);
+
+	if (hFile == INVALID_HANDLE_VALUE)
+	{
+		errorPrinter(L"file open error!");
+		goto exit;
+	}
 	goto exit;
 
 exit:
+
+	if (INVALID_HANDLE_VALUE != hFile)
+	{
+		CloseHandle(hFile);
+	}
 	return retVal;
 }
