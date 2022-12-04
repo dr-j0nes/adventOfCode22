6d4a9fce8e564d28117b790bac074b1658e989c8
Dr. Jones <jonathan.matthew.holmes@gmail.com>
2022-12-04 00:35:22 -0500

    formatting fixes, some cuteness with error codes
    Signed-off-by: Dr. Jones <jonathan.matthew.holmes@gmail.com>

--- adventOfCode/Source.c
+++ adventOfCode/Source.c
@@ -1,37 +1,43 @@
 #include <Windows.h>
 #include <stdio.h>
+
 //
 // need to be static for REASONS
 // 0x1000 = 4096 = 1 page (ok not always but always)
 //
 #define BUFFERSIZE 0x1000
 
+//
+// ngl kinda don't like this name
+//
 void errorPrinter(LPTSTR printer)
 {
 	DWORD dwError = 0;
-
 	dwError = GetLastError();
 	printf("%ls\n", printer);
 	printf("ERROR\t%d\n", dwError);
-
 }
 
 int main() {
-	DWORD retVal = 0;
+	DWORD retVal = 1111;
 	BOOL bCheck = FALSE;
 	DWORD dwBytesRead = 0;
 	LPDWORD pBytesRead = &dwBytesRead;
-	LPVOID lpBuffer = malloc(BUFFERSIZE);
-	DWORD dwCurrentSize = 0;
+	LPVOID lpBuffer = malloc(BUFFERSIZE); // CAN FAIL CHECK THIS
+	DWORD dwCurrBuffSize = 0;
 	HANDLE hFile = INVALID_HANDLE_VALUE;
 
 	if (lpBuffer == NULL)
 	{
-		errorPrinter(L"malloc error!");
+		errorPrinter(L"malloc error everything is fucked!");
+		retVal = 2222;
 		goto exit;
 	}
 	RtlSecureZeroMemory(lpBuffer, BUFFERSIZE); // not strictly necessary but whatever
 
+	//
+	// open read....
+	//
 	hFile = CreateFile(
 		L"../XXXInput/day1pt1.txt",
 		GENERIC_READ,
@@ -44,8 +50,14 @@ int main() {
 	if (hFile == INVALID_HANDLE_VALUE)
 	{
 		errorPrinter(L"file open error!");
+		retVal = 3333;
 		goto exit;
 	}
+
+	//
+	// the one we hit when things are good
+	//
+	retVal = 0;
 	goto exit;
 
 exit:
